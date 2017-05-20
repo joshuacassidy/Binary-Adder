b11346ffa53135e7311a6347ca867eaa81285087
joshuacassidy <Joshcassidy79@gmail.com>
2017-05-20 12:56:32 +0100

    initial commit

---
+++ BinaryAdder/BinaryAdder.js
@@ -0,0 +1,34 @@
+(function main(num1,num2,z){
+  var x = (""+num1).split("");
+  var y = (""+num2).split("");
+
+function ConvertToEightBit(convert){
+  convert.reverse();
+   for(var i = convert.length; i < 8; i++){
+    convert.push(0);
+  }
+}
+
+ConvertToEightBit(x);
+ConvertToEightBit(y);
+ConvertToEightBit(z);
+ 
+function ConvertToNumber(convert,convert2){
+  for(var i = 0; i < 8; i++){
+    convert[i] = parseInt(convert[i]);
+    convert2[i] = parseInt(convert2[i]);
+  }
+}
+ConvertToNumber(x,y);
+
+function Add(x,y,z){
+  for(var i = 0; i < 8;i++){
+    z[i+1] = x[i] && z[i] || x[i] && y[i] || y[i] && z[i] || x[i] && y[i] && z[i];
+   [i]= x[i] && !y[i] && !z[i] || !x[i] && y[i] && !z[i] || !x[i] && !y[i] && z[i] || x[i] && y[i] && z[i];  z 
+    z[i] === true  ? z[i] = 1 : z[i];  
+  }
+  z = z.reverse().join("");
+  alert(z);
+}
+Add(x,y,z);
+})(parseInt(prompt("First Number?")),parseInt(prompt("Second Number")),[]);
