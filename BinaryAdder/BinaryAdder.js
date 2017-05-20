f347431bc4e7a80937157268986b83c6534d6c39
joshuacassidy <Joshcassidy79@gmail.com>
2017-05-20 14:59:50 +0100

    Added functionality for adding 2 binary numbers together larger than 8 bits

--- BinaryAdder/BinaryAdder.js
+++ BinaryAdder/BinaryAdder.js
@@ -1,28 +1,30 @@
 (function main(num1,num2,z){
   var x = (""+num1).split("");
   var y = (""+num2).split("");
+  var len = 0;
+function ConvertToEightBit(convert,len){
 
-function ConvertToEightBit(convert){
   convert.reverse();
-   for(var i = convert.length; i < 8; i++){
+   for(var i = convert.length; i < len; i++){
     convert.push(0);
   }
 }
+x.length > y.length ? len = x.length : len = y.length;  
 
-ConvertToEightBit(x);
-ConvertToEightBit(y);
-ConvertToEightBit(z);
+ConvertToEightBit(x,len);
+ConvertToEightBit(y,len);
+ConvertToEightBit(z,len);
  
-function ConvertToNumber(convert,convert2){
-  for(var i = 0; i < 8; i++){
+function ConvertToNumber(convert,convert2,len){
+  for(var i = 0; i < len; i++){
     convert[i] = parseInt(convert[i]);
     convert2[i] = parseInt(convert2[i]);
   }
 }
-ConvertToNumber(x,y);
+ConvertToNumber(x,y,len);
 
-function Add(x,y,z){
-  for(var i = 0; i < 8;i++){
+function Add(x,y,z,len){
+  for(var i = 0; i < len;i++){
     //Most Significant Digit
     z[i+1] = x[i] && z[i] || x[i] && y[i] || y[i] && z[i] || x[i] && y[i] && z[i];
     //Least Significant Digit
@@ -30,8 +32,9 @@ function Add(x,y,z){
     //Converting True value back to Binary  
     z[i] === true  ? z[i] = 1 : z[i];  
   }
+  z[z.length-1] === 0 ? z.pop() : z;
   z = z.reverse().join("");
   alert(z);
 }
-Add(x,y,z);
-})(parseInt(prompt("num?")),parseInt(prompt("num?")),[]);
+Add(x,y,z,len);
+})(parseInt(prompt("First Number?")),parseInt(prompt("Second Number?")),[]);
